3ff7a995284d5becc34ab25557b168583b3ab436
Daniel T Nunes <nnsdtr.github@gmail.com>
2020-09-20 20:53:07 -0300

    new file: criação de classe para espectadores

---
+++ src/Spectator.java
@@ -0,0 +1,14 @@
+public class Spectator {
+   public String cpf;
+   public String name;
+   public String login;
+
+   /**
+    * Método construtor
+    */
+   public Spectator() {
+      this.cpf = "";
+      this.name = "";
+      this.login = "";
+   }
+}
