8b3eb2393ea842ee365bcbcd63453473c3fbf557
Daniel T Nunes <nnsdtr.github@gmail.com>
2020-10-04 17:16:49 -0300

    modified: Método de carregamento dos dados dos Espectadores foi deslocado para EspecLista.

--- src/App.java
+++ src/App.java
@@ -1,29 +1,13 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class App {
-   public static ListaEspectadores lerEspectadores(String caminho) throws FileNotFoundException {
-      Scanner leitorArq = new Scanner(new File(caminho));
+   public static void main(String[] args) throws FileNotFoundException {
+      EspecLista dbEspectadores = new EspecLista();
+      dbEspectadores.carregar("fake_data.txt");
 
-      ListaEspectadores espectadores = new ListaEspectadores();
+      Espectador espectador = dbEspectadores.localizar("123.755.737-22");
 
-      while(leitorArq.hasNextLine()) {
-         String[] aux = leitorArq.nextLine().split(";");
-
-         Espectador novo = new Espectador();
-         novo.cpf = aux[0];
-         novo.nome = aux[1];
-         novo.login = aux[2];
-
-         espectadores.inserir(novo);
-      }
-
-      return espectadores;
-   }
-
-   public static void main(String[] args) {
-
+      System.out.println(espectador.nome + " <" + espectador.login + ">");
    }
 }
 
