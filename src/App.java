b9d511f44856d080d25031b2ff972926f1167290
Daniel T Nunes <nnsdtr.github@gmail.com>
2020-10-31 14:22:30 -0300

    modified: reestruturando o trabalho. Foram deletados os scripts da estrutura de Lista para Espectador.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class App {
-
-   public static void main(String[] args) throws FileNotFoundException {
+   public static void main(String[] args) {
 
    }
 }
