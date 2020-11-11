91395697d09246caee59cdcec42ef4805b85bd8c
Rafael Santos <40042652+rafaelsnv@users.noreply.github.com>
2020-11-11 02:08:36 -0300

    modified: implementado método de carregamento de séries.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,14 @@
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
-public class App {
 
-   static Random sorteio = new Random(46);
+public class App {
 
 //   public static AvlEspec carregar(String caminho) throws FileNotFoundException {
 //      Scanner leitorArq = new Scanner(new File(caminho));
@@ -26,7 +30,27 @@ public class App {
 //      return arvore;
 //   }
 
-   public static void main(String[] args) {
+   public static AvlSeriesLancadas carregarSeriesLancadas (String caminho) throws FileNotFoundException {
+      Scanner leitorArq = new Scanner(new File(caminho));
+      AvlSeriesLancadas seriesLancadas = new AvlSeriesLancadas();
+      DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
+
+      while (leitorArq.hasNextLine()){
+         String [] aux = leitorArq.nextLine().split(";");
+
+         Series nova = new Series();
+         nova.nome = aux[0];
+         nova.dataDeLancamento = LocalDate.parse(aux[1], formatador);
+         nova.numeroTotalEpisodios = Integer.parseInt(aux[2]);
+
+         seriesLancadas.inserir(nova);
+      }
+      return seriesLancadas;
+   }
+
+   static Random sorteio = new Random(46);
+
+   public static void main(String[] args) throws FileNotFoundException {
       // Teste da AVL de Espectadores
       AvlEspec arvEspectadores = new AvlEspec();
 
@@ -47,8 +71,6 @@ public class App {
       System.out.println(arvoreToda);
 
       // Teste da AVL de Series
-
-
-
+      AvlSeriesLancadas testeAVL = carregarSeriesLancadas("strend/series_FakeData.txt");
    }
 }
